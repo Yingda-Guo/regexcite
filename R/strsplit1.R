7ed50072867c517154297c3e28d0ea4648fe061d
yingda-guo <yingda.guo@capgemini.com>
2023-01-12 15:26:29 -0400

    add function description

--- R/strsplit1.R
+++ R/strsplit1.R
@@ -1,3 +1,14 @@
+#' Split a String
+#'
+#' @param x A character vector with one element
+#' @param split A string to be spitted
+#'
+#' @return A character vector
+#' @export
+#'
+#' @examples
+#' x <- "alfa,bravo,charlie,delta"
+#' strsplit1(x, split = ",")
 strsplit1 <- function(x, split) {
   strsplit(x, split = split)[[1]]
 }
